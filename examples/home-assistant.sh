7fb79b89e0d1310d734984119d87ffa5d8ae233f
Robert Schindler <r.schindler@efficiosoft.com>
2019-01-12 13:27:27 +0100

    Added example for Home Assistant

---
+++ examples/home-assistant.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+#
+# This can be used to authenticate to Home Assistant with the (not yet
+# released) command_line auth provider.
+#
+# The configuration.yaml entry might look as follows:
+#
+#     homeassistant:
+#       auth_providers:
+#       - type: command_line
+#         command: /path/to/this/script
+#         # Uncomment and see below if you want the Home Assistant
+#         # user to be populated with his full name.
+#         #meta: true
+#
+
+# curl is available in the official home-assistant docker image
+CLIENT="curl"
+
+SERVER="ldap://ldap-server:389"
+USERDN="uid=$username,ou=people,dc=example,dc=com"
+
+BASEDN="$USERDN"
+SCOPE="base"
+FILTER="(&(objectClass=person)(memberOf=cn=smarthome,ou=groups,dc=example,dc=com))"
+
+TIMEOUT=3
+
+
+########## Home Assistant specific features
+# Have the value of cn be set as user's friendly name in Home Assistant.
+NAME_ATTR="cn"
+ATTRS="$NAME_ATTR"
+
+on_auth_success() {
+	# print the meta entries for use in HA
+	name=$(sed -nr "s/^\s*$NAME_ATTR:\s*(.+)\s*\$/\1/Ip") <<-EOF
+		$output
+		EOF
+	[ -z "$name" ] || echo "name=$name"
+}
+
+
+. "$(dirname "$0")/../ldap-auth.sh"
